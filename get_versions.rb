d1218819efac6053974796fb88db4002004a88af
B1nj0y <v@joyv.net>
2017-12-09 16:58:12 +0800

    remove the big head V

--- get_versions.rb
+++ get_versions.rb
@@ -4,7 +4,7 @@ require 'json'
 def get_versions()
   api_url = 'https://api.github.com/repos/coq/coq/releases'
   JSON.parse(RestClient.get api_url || []).sort_by {|r| r["created_at"]}.map do |r|
-    r["tag_name"] || ""
+    r["tag_name"].sub(/^V/, "") || ""
   end
 end
 
