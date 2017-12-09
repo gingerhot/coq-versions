fb1a05208d1864bb17d7a713617e6779e3b1d6d8
B1nj0y <v@joyv.net>
2017-12-09 13:57:27 +0800

    fix the release order

--- get_versions.rb
+++ get_versions.rb
@@ -3,8 +3,8 @@ require 'json'
 
 def get_versions()
   api_url = 'https://api.github.com/repos/coq/coq/releases'
-  JSON.parse(RestClient.get api_url || []).map do |i|
-    i["tag_name"] || ""
+  JSON.parse(RestClient.get api_url || []).sort_by {|r| r["created_at"]}.map do |r|
+    r["tag_name"] || ""
   end
 end
 
