905e82943c6503f7ae3c89fc89a46774a8315edf
B1nj0y <idegorepl@gmail.com>
2019-10-06 17:41:00 +0800

    update script

--- get_versions.rb
+++ get_versions.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 require 'json'
 
 def get_versions()
-  api_url = 'https://api.github.com/repos/coq/coq/releases'
+  api_url = 'https://api.github.com/repos/coq/coq/releases?page=1&per_page=50'
   JSON.parse(RestClient.get api_url || []).sort_by {|r| r["created_at"]}.map do |r|
     r["tag_name"].sub(/^V/, "") || ""
   end
